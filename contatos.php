e843e2ca434b313370a7b7bab60ef4a633f780bc
Diogo-Alves0 <87331499+Diogo-Alves0@users.noreply.github.com>
2023-12-05 20:19:28 -0300

    Add files via upload

---
+++ contatos.php
@@ -0,0 +1,101 @@
+<?php  include_once("cabecalho.php") ?>
+
+      
+   
+      <!-- Contact Form and Gmap-->
+      <section class="section section-md section-last bg-default text-md-left">
+        <div class="container">
+          <div class="row row-50">
+            <div class="col-lg-6 section-map-small">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.0873358331405!2d-46.694789199999995!3d-23.565306600000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce57a6b4e03d11%3A0x4bf8b26fc002c96!2sAv.%20Brig.%20Faria%20Lima%2C%20750%20-%20Pinheiros%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2005426-200!5e0!3m2!1spt-BR!2sbr!4v1701408232920!5m2!1spt-BR!2sbr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-lg-6">
+              <h4 class="text-spacing-50">Contate-nos</h4>
+              <form class="rd-form rd-mailform" data-form-output="form-output-global" data-form-type="contact" method="post" action="enviar.php">
+                <div class="row row-14 gutters-14">
+                  <div class="col-sm-6">
+                    <div class="form-wrap">
+                      <input class="form-input" id="contact-first-name" type="text" name="name" placeholder="Nome e Sobrenome" data-constraints="@Required">
+                      <label class="form-label" for="contact-first-name">Nome</label>
+                    </div>
+                  </div>
+
+                    <div class="col-sm-6">
+                    <div class="form-wrap">
+                      <input class="form-input" id="contact-tel" type="telefone" name="telefone">
+                      <label class="form-label" for="contact-tel">WhatsApp</label>
+                    </div>
+                  </div>
+
+                  <div class="col-12">
+                    <div class="form-wrap">
+                      <input class="form-input" id="contact-email" type="email" name="email" data-constraints="@Email @Required">
+                      <label class="form-label" for="contact-email">E-mail</label>
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <div class="form-wrap">
+                      <label class="form-label" for="contact-message">Menssagem</label>
+                      <textarea class="form-input" id="contact-message" name="message" data-constraints="@Required"></textarea>
+                    </div>
+                  </div>
+                </div>
+                <button class="button button-primary button-pipaluk" type="submit">Enviar</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+
+   <!-- Contact information-->
+      <section class="section section-md section-first bg-default">
+        <div class="container">
+          <div class="row row-30 justify-content-center">
+            <div class="col-sm-8 col-md-6 col-lg-4">
+              <article class="box-contacts">
+                <div class="box-contacts-body">
+                  <div class="box-contacts-icon fl-bigmug-line-cellphone55"></div>
+                  <div class="box-contacts-decor"></div>
+                  <p class="box-contacts-link"><a href="tel:#">11 3455-4688</a></p>
+                </div>
+              </article>
+            </div>
+            <div class="col-sm-8 col-md-6 col-lg-4">
+              <article class="box-contacts">
+                <div class="box-contacts-body">
+                  <div class="box-contacts-icon fl-bigmug-line-up104"></div>
+                  <div class="box-contacts-decor"></div>
+                  <p class="box-contacts-link"><a href="#">Av. Brigadeiro faria lima 750, São Paulo</a></p>
+                </div>
+              </article>
+            </div>
+            <div class="col-sm-8 col-md-6 col-lg-4">
+              <article class="box-contacts">
+                <div class="box-contacts-body">
+                  <div class="box-contacts-icon fl-bigmug-line-chat55"></div>
+                  <div class="box-contacts-decor"></div>
+                  <p class="box-contacts-link"><a href="mailto:#">coffee.house@gmail.org</a></p>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+     <?php include_once("rodape.php")?>
+
+    </div>
+    <!-- Global Mailform Output-->
+    <div class="snackbars" id="form-output-global"></div>
+    <!-- Javascript-->
+    <script src="js/core.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/
+jquery.mask/1.14.11/jquery.mask.min.js"></script>
+
+<script src="js/mascaras.js"></script>
